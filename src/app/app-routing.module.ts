b335672767be52530d57ea5ad6ac3ec5b0aa71ce
kolade Afeez <daisedalok@gmail.com>
2020-01-29 16:13:03 +0100

    about page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: HomeComponent },
+  {path: 'about', component: AboutComponent},
+{ path: '',  component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
